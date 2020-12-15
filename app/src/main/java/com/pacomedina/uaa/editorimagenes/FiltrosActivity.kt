0dc4c3bb16ca0ee60d9a42d5b100c0120ec5c451
Rodrigo Maafs <rodrigoelmaps@gmail.com>
2020-12-14 23:22:08 -0600

    Funcionalidad para el botón para guardar imagen

--- app/src/main/java/com/pacomedina/uaa/editorimagenes/FiltrosActivity.kt
+++ app/src/main/java/com/pacomedina/uaa/editorimagenes/FiltrosActivity.kt
@@ -5,12 +5,16 @@ import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
 import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
 import com.pacomedina.uaa.editorimagenes.filtros.*
+import java.io.File
+import java.io.FileOutputStream
 
 
 class FiltrosActivity : AppCompatActivity() {
@@ -20,7 +24,7 @@ class FiltrosActivity : AppCompatActivity() {
     private lateinit var btnDeshacer: Button
     private lateinit var btnAceptar: Button
     private lateinit var imgFoto: ImageView
-    private lateinit var layoutContainer : LinearLayout;
+    private lateinit var layoutContainer: LinearLayout;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,9 +42,15 @@ class FiltrosActivity : AppCompatActivity() {
         imgFoto.setImageURI(Uri.parse(strUser))
 
         //Guardará la imagen previa antes de meter el próximo filtro
-        var bitmapUndo : Bitmap = (imgFoto.getDrawable() as BitmapDrawable).bitmap.copy((imgFoto.getDrawable() as BitmapDrawable).bitmap.config, true)
+        var bitmapUndo: Bitmap = (imgFoto.getDrawable() as BitmapDrawable).bitmap.copy(
+            (imgFoto.getDrawable() as BitmapDrawable).bitmap.config,
+            true
+        )
         //Guardará la imagen tal y como cuando la seleccionó de la galeria o desde la cámara
-        var bitmapOriginal : Bitmap = (imgFoto.getDrawable() as BitmapDrawable).bitmap.copy((imgFoto.getDrawable() as BitmapDrawable).bitmap.config, true)
+        var bitmapOriginal: Bitmap = (imgFoto.getDrawable() as BitmapDrawable).bitmap.copy(
+            (imgFoto.getDrawable() as BitmapDrawable).bitmap.config,
+            true
+        )
 
         //Regresamos al main
         btnRegresar.setOnClickListener {
@@ -54,12 +64,21 @@ class FiltrosActivity : AppCompatActivity() {
 
         //Botón para "Aceptar"
         btnAceptar.setOnClickListener {
-            bitmapUndo = (imgFoto.getDrawable() as BitmapDrawable).bitmap.copy((imgFoto.getDrawable() as BitmapDrawable).bitmap.config, true)
+            bitmapUndo = (imgFoto.getDrawable() as BitmapDrawable).bitmap.copy(
+                (imgFoto.getDrawable() as BitmapDrawable).bitmap.config,
+                true
+            )
             Toast.makeText(this, "Filtro guardado.", Toast.LENGTH_SHORT).show()
         }
 
+        //Botón para "Guardar"
+        btnGuardar.setOnClickListener {
+            guardarImagen()
+        }
+
         //Añadimos nuestro arreglo de filtros que usará la app
-        var filtros = arrayOf(FiltroControlador(this).setFiltro(FiltroNegativo()),
+        var filtros = arrayOf(
+            FiltroControlador(this).setFiltro(FiltroNegativo()),
             FiltroControlador(this).setFiltro(FiltroGrises()),
             FiltroControlador(this).setFiltro(FiltroBrillo()),
             FiltroControlador(this).setFiltro(FiltroContraste()),
@@ -67,7 +86,7 @@ class FiltrosActivity : AppCompatActivity() {
             FiltroControlador(this).setFiltro(FiltroSeparacionRojo()),
             FiltroControlador(this).setFiltro(FiltroSeparacionVerde()),
             FiltroControlador(this).setFiltro(FiltroSeparacionAzul())
-            )
+        )
 
         //Ciclamos todo el arreglo
         for (filtro in filtros) {
@@ -81,5 +100,39 @@ class FiltrosActivity : AppCompatActivity() {
         }
 
     }
+
+    /**
+     * Función que guarda la imagen y abre su vista.
+     */
+    private fun guardarImagen() {
+        //Guardamos la imagen
+        val draw = imgFoto.getDrawable() as BitmapDrawable
+        val bitmap = draw.bitmap
+
+        var outStream: FileOutputStream? = null
+        val sdCard: File = Environment.getExternalStorageDirectory()
+        val dir = File(sdCard.getAbsolutePath().toString() + "/editor-isc")
+        dir.mkdirs()//Creamos la carpeta en caso de que no exista para guardar las fotos editadas de esta APP
+        val fileName =
+            String.format("%d.png", System.currentTimeMillis())//Nombre
+        val outFile = File(dir, fileName)
+        outStream = FileOutputStream(outFile)
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, outStream)
+        outStream.flush()
+        outStream.close()
+
+        //Abrimos la imagen guardada
+        val install = Intent(Intent.ACTION_VIEW)
+        install.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK
+        install.setDataAndType(Uri.fromFile(outFile), "image/*")
+        val apkURI = FileProvider.getUriForFile(
+            this, this.getApplicationContext()
+                .getPackageName().toString() + ".provider", outFile
+        )
+        install.setDataAndType(apkURI, "image/*")
+        install.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+        this.startActivity(install)
+    }
+
 
 }
