71e06b1c048f428e96b17f6ca8610bbe1c902378
Rodrigo Maafs <rodrigoelmaps@gmail.com>
2020-12-14 22:00:49 -0600

    Se creo el filtro para Contraste

--- app/src/main/java/com/pacomedina/uaa/editorimagenes/filtros/FiltroContraste.kt
+++ app/src/main/java/com/pacomedina/uaa/editorimagenes/filtros/FiltroContraste.kt
@@ -1,7 +1,6 @@
 package com.pacomedina.uaa.editorimagenes.filtros
 
 import android.graphics.Bitmap
-import android.graphics.Color
 import android.graphics.drawable.BitmapDrawable
 import android.widget.ImageView
 import com.pacomedina.uaa.editorimagenes.R
@@ -28,9 +27,13 @@ class FiltroContraste : Filtro {
         for (y in 0 until pich) {
             for (x in 0 until picw) {
                 val index: Int = y * picw + x
-                val R = pix[index] shr 16 and 0xff
-                val G = pix[index] shr 8 and 0xff
-                val B = pix[index] and 0xff
+                var R = pix[index] shr 16 and 0xff
+                var G = pix[index] shr 8 and 0xff
+                var B = pix[index] and 0xff
+
+                R = procesar(R)
+                G = procesar(G)
+                B = procesar(B)
 
                 pix[index] = -0x1000000 or (R shl 16) or (G shl 8) or B
             }
@@ -38,5 +41,21 @@ class FiltroContraste : Filtro {
 
         bitmap.setPixels(pix, 0, bitmap.getWidth(), 0, 0, bitmap.getWidth(), bitmap.getHeight());
         return bitmap
+    }
+
+    private fun procesar(N: Int) : Int {
+        val CONTRASTE = 50
+        var pixel = 0.0
+        var contrast: Double = (100.0 + CONTRASTE) / 100.0
+        contrast *= contrast
+
+        pixel = N / 255.0
+        pixel -= 0.5
+        pixel *= contrast
+        pixel += 0.5
+        pixel *= 255.0
+        if (pixel < 0) pixel = 0.0
+        if (pixel > 255) pixel = 255.0
+        return pixel.toInt()
     }
 }
