2b583b8cae01dbaf27269cf1b81f9d309aa2d9da
Rodrigo Maafs <rodrigoelmaps@gmail.com>
2020-12-16 18:56:54 -0600

    Se corrigió el filtro Edge Detection

--- app/src/main/java/com/pacomedina/uaa/editorimagenes/filtros/FiltroBlurEdgeDetection.kt
+++ app/src/main/java/com/pacomedina/uaa/editorimagenes/filtros/FiltroBlurEdgeDetection.kt
@@ -21,24 +21,13 @@ class FiltroBlurEdgeDetection : Filtro {
 
         val convolucion = ConvolucionMatriz()
         val array = arrayOf(
-            intArrayOf(1, 1, 1),
+            intArrayOf(1, 0, -1),
             intArrayOf(0, 0, 0),
-            intArrayOf(-1, -1, -1)
+            intArrayOf(-1, 0, 1)
         )
         convolucion.setMatriz(array)
-        convolucion.Factor = 1
+        convolucion.Factor = 3
         convolucion.Offset = 127
         return convolucion.convolucion(bitmap)
-
-//        val matrix = ConvolutionMatrix(3)
-//        val config = arrayOf(
-//            intArrayOf(1, 1, 1),
-//            intArrayOf(0, 0, 0),
-//            intArrayOf(-1, -1, -1)
-//        )
-//        matrix.applyConfig(config)
-//        matrix.Factor = 1
-//        matrix.Offset = 127
-//        return matrix.computeConvolution3x3(bitmap, matrix)
     }
 }
